35e5ba59b16798c6df0d7c73eea789e5b2b00e8d
tyswartz77 <70165023+tyswartz77@users.noreply.github.com>
2020-08-31 20:26:35 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hypotenuse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int S1 = 3;
+            int S2 = 4;
+            double H = Math.Sqrt((S1 * S1) + (S2 * S2));
+            Console.WriteLine("The value of the hypotenuse is " + H);
+        }
+    }
+}
