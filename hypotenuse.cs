211ba303d8aa72160fb28b5af44c08c901b2449b
tyswartz77 <70165023+tyswartz77@users.noreply.github.com>
2020-09-09 11:56:08 -0400

    Add files via upload
    hypotenuse program

---
+++ hypotenuse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter the length of the first side");
+            int S1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the length of the second side");
+            int S2 = Convert.ToInt32(Console.ReadLine());
+            double H = Math.Sqrt((S1 * S1) + (S2 * S2));
+            Console.WriteLine("The value of the hypotenuse is " + H);
+        }
+    }
+}
